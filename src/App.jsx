cf9c030d894d34036766d6dd8a32c85de79fdf20
Phan Anh <phananh12042003@gmail.com>
2023-07-06 23:45:47 -0400

    styled components 🚀

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,53 @@
+import styled from "styled-components";
+import Button from "./ui/Button";
+import GlobalStyles from "./styles/GlobalStyles";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyledApp = styled.div`
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello WOrld</div>;
+  return (
+    <>
+      <GlobalStyles></GlobalStyles>
+
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button
+                variation="primary"
+                size="medium"
+                onClick={() => alert("Check in")}
+              >
+                Check in
+              </Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check in")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="nymber" placeholder="Number of guests"></Input>
+              <Input type="nymber" placeholder="Number of guests"></Input>
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
