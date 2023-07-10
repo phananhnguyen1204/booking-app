5afd7f33982df150b75b27c93e80ce3ae6a57c31
Phan Anh <phananh12042003@gmail.com>
2023-07-09 22:14:53 -0400

    load spinner🚀

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,10 +1,26 @@
+import styled from "styled-components";
 import { useUser } from "../features/authentication/useUser";
+import Spinner from "../ui/Spinner";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 function ProtectedRoute({ children }) {
   //1. Load the authenticated user
   const { user, isLoading } = useUser();
 
   //2. While loading, show a spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner></Spinner>
+      </FullPage>
+    );
 
   //3. If there is NO authenticated user, redirect to the /login
 
