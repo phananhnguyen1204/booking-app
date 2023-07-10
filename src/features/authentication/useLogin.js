511113440334d3c4aa9cdf7af92bc4286501da6b
Phan Anh <phananh12042003@gmail.com>
2023-07-09 22:49:44 -0400

    set email password 🚀

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,14 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { login as loginApi } from "../../services/apiAuth";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     //receive data from onSuccess handler
     onSuccess: (user) => {
+      //manually set data into React Query Cache
+      queryClient.setQueryData(["user"], user);
       navigate("/dashboard");
     },
     onError: (err) => {
